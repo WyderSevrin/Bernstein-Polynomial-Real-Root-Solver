43c6dd6465449cbd6aaea32a0c722eaa69fac097
Mortpo <anquetin.r@gmail.com>
2020-01-11 15:00:33 +0100

    test

--- Bernstein/Bernstein-test.cpp
+++ Bernstein/Bernstein-test.cpp
@@ -3,9 +3,9 @@
 int main()
 {
 
-    Bernstein poly(4, 4, 4, -4, -4, -5); //creation de la classe polynome avec un polynome generateur
-    poly.root(0.0000001);                //trouve les racines
-    poly.showracine();                   //affiche les racines
+    Bernstein poly(1, 109.5, 2416.0, -3985.5, -61025.0, 31200.0); //creation de la classe polynome avec un polynome generateur
+    poly.root(0.0000001);                                         //trouve les racines
+    poly.showracine();                                            //affiche les racines
 
     return 0;
 }
